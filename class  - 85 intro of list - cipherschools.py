95e0f206bdd74320b87ba8f5da1359ce4c0ce74f
Ishayadav25 <115414374+Ishayadav25@users.noreply.github.com>
2022-12-18 13:21:27 +0530

    Add files via upload

---
+++ class  - 85 intro of list - cipherschools.py
@@ -0,0 +1,15 @@
+numbers = [1,2,3,4]
+print(numbers)
+
+mixed = [1,2,"word"]
+print(mixed[-1])
+
+mixed[1] = 'two'
+print(mixed)
+
+
+# how to add data in list
+
+# fruits = ['grapes', 'apple']
+# fruits.append('mango')
+# print(fruits)
